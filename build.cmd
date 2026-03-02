b376d81c85cd30cee1f3716bbfbb8e093842ca7f
Werner Fouché <werner.fouche@gmail.com>
2026-03-02 08:29:13 +0200

    Support command-line arguments

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
 del /Q .\jbx.exe
 del /Q hello.java
 nim c -d:ssl -d:release jbx.nim
-.\jbx.exe
+.\jbx.exe hello@jbangdev John
 del /Q hello.java
--- jbx.nim
+++ jbx.nim
@@ -1,12 +1,15 @@
 import std/[httpclient, json]
 import osproc
+import strutils
+import std/cmdline
 
 # 1. Initialize the client
 let client = newHttpClient()
 
+let parts: seq[string] = split(paramStr(1), '@')
 try:
-  let alias = "hello"
-  let baseUrl = "https://raw.githubusercontent.com/jbangdev/jbang-catalog/refs/heads/main"
+  let alias = parts[0]
+  let baseUrl = "https://raw.githubusercontent.com/" & parts[1] & "/jbang-catalog/refs/heads/main"
 
   # 2. Retrieve the content
   let url = baseUrl & "/" & "jbang-catalog.json"
@@ -22,7 +25,7 @@ try:
   let filename = scriptRef
   writeFile(filename, scriptText)
 
-  discard execCmd("java " & filename & " John")
+  discard execCmd("java " & filename & " " & paramStr(2))
 
 finally:
   client.close()
