4fd39105d2fd2343e964398422c16fa9b4fd4743
Werner Fouché <werner.fouche@gmail.com>
2026-03-02 08:46:11 +0200

    Added optional types

--- jbx.nim
+++ jbx.nim
@@ -4,25 +4,25 @@ import strutils
 import std/cmdline
 
 # 1. Initialize the client
-let client = newHttpClient()
+let client: HttpClient = newHttpClient()
 
 let parts: seq[string] = split(paramStr(1), '@')
 try:
-  let alias = parts[0]
-  let baseUrl = "https://raw.githubusercontent.com/" & parts[1] & "/jbang-catalog/refs/heads/main"
+  let alias: string = parts[0]
+  let baseUrl: string = "https://raw.githubusercontent.com/" & parts[1] & "/jbang-catalog/refs/heads/main"
 
   # 2. Retrieve the content
-  let url = baseUrl & "/" & "jbang-catalog.json"
-  let response = client.getContent(url)
+  let url: string = baseUrl & "/" & "jbang-catalog.json"
+  let response: string = client.getContent(url)
 
   # 3. Parse the JSON string
-  let data = parseJson(response)
+  let data: JsonNode = parseJson(response)
 
   # 4. Access fields
-  let scriptRef = data["aliases"][alias]["script-ref"].getStr()
-  let scriptUrl = baseUrl & "/" & scriptRef
-  let scriptText = client.getContent(scriptUrl)
-  let filename = scriptRef
+  let scriptRef: string = data["aliases"][alias]["script-ref"].getStr()
+  let scriptUrl: string = baseUrl & "/" & scriptRef
+  let scriptText: string = client.getContent(scriptUrl)
+  let filename: string = scriptRef
   writeFile(filename, scriptText)
 
   discard execCmd("java " & filename & " " & paramStr(2))
